b0fff0d14566156d324545280156b3317fc503de
luisin925 <73363958+luisin925@users.noreply.github.com>
2023-04-18 17:43:56 -0600

    Adding my code for stairs

---
+++ Retos/Reto #16 - LA ESCALERA [Media]/javascript/stairAngel.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let cantStairs = +prompt("Numero","");
+
+let stair = '__';
+
+console.log(stairs(cantStairs));
+
+
+function stairs(num){
+    if(num > 0){
+        for(let i = 0; i <= num; i++){
+            console.log(' '.repeat(num - i)+'_|');
+            console.log('\n');
+        }
+    }else if(num < 0){
+        num = Math.abs(num);
+        for(let i = num; i > 0; i--){
+            console.log(' '.repeat(num - i) + '|_');
+            console.log('\n');
+        }
+    }else return stair;
+}
